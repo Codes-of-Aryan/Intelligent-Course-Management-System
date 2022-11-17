e85f287e72b0b3232a13f0b816a4c66ed2a04ba5
BhatiaDivtej <divtej17@connect.hku.hk>
2022-11-18 02:02:41 +0800

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_mysql_connector import MySQL
 
 app = Flask(__name__)
+
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_DATABASE'] = 'sys'
 mysql = MySQL(app)
